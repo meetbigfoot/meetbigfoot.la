4fdece47bcc46d5c12815f3ba0729782b8379214
Matt Born <mattborn@users.noreply.github.com>
2023-04-28 08:41:00 -0600

    sudo make sexier

--- scripts.js
+++ scripts.js
@@ -7,11 +7,27 @@ g('watch').addEventListener('click', e => {
   const iframe = document.createElement('iframe')
   iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
   iframe.allowfullscreen = true
-  iframe.src = 'https://www.youtube.com/embed/ScMzIvxBSi4?autoplay=1'
+  iframe.src = 'https://www.youtube.com/embed/At3kSthUM_k?autoplay=1'
   g('softbox').appendChild(iframe)
+
+  ScrollReveal().reveal('#softbox', {
+    cleanup: true,
+    // container: '#softbox',
+    distance: '50%',
+    interval: 100,
+    origin: 'bottom',
+  })
 })
 
 g('popup').addEventListener('click', e => {
   e.target.style.display = 'none'
   g('softbox').innerHTML = ''
+})
+
+ScrollReveal().reveal('h1, h2, #getWaitlistContainer, #watch, a', {
+  cleanup: true,
+  container: '#stack',
+  distance: '50%',
+  interval: 200,
+  origin: 'bottom',
 })
