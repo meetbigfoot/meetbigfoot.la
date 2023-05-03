8669896650d182f9862cd45f439af759e3126b4c
Matt Born <mattborn@users.noreply.github.com>
2023-05-03 15:42:20 -0600

    Add clarity.ms

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+;((c, l, a, r, i, t, y) => {
+  c[a] =
+    c[a] ||
+    function () {
+      ;(c[a].q = c[a].q || []).push(arguments)
+    }
+  t = l.createElement(r)
+  t.async = 1
+  t.src = 'https://www.clarity.ms/tag/' + i
+  y = l.getElementsByTagName(r)[0]
+  y.parentNode.insertBefore(t, y)
+})(window, document, 'clarity', 'script', 'gyd8px7h8m')
+
 const g = document.getElementById.bind(document)
 const q = document.querySelectorAll.bind(document)
 
