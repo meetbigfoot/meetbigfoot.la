86a1919510005cfd299a93ff5f85e604ac9ca844
Matt Born <mattborn@users.noreply.github.com>
2023-05-04 10:23:44 -0600

    Simplify via James’ feedback

--- scripts.js
+++ scripts.js
@@ -14,34 +14,6 @@
 const g = document.getElementById.bind(document)
 const q = document.querySelectorAll.bind(document)
 
-g('watch').addEventListener('click', e => {
-  document.body.style.overflow = 'hidden'
-  g('popup').style.display = 'flex'
-
-  const video = document.createElement('video')
-  video.allowfullscreen = true
-  video.autoplay = true
-  video.playsinline = true
-  video.src =
-    'https://res.cloudinary.com/bigfoot-cdn/video/upload/v1683137674/demo/littlefoot-teaser-draft-2-compressed_qadeai.mp4'
-  g('softbox').appendChild(video)
-  video.play()
-
-  ScrollReveal().reveal('#softbox', {
-    cleanup: true,
-    // container: '#softbox',
-    distance: '50%',
-    interval: 100,
-    origin: 'bottom',
-  })
-})
-
-g('popup').addEventListener('click', e => {
-  document.body.style.overflow = 'unset'
-  e.target.style.display = 'none'
-  g('softbox').innerHTML = ''
-})
-
 ScrollReveal().reveal('h1, h2, #getWaitlistContainer, #watch, a', {
   cleanup: true,
   container: '#stack',
